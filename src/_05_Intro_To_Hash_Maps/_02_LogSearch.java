b498bc7622ab14b471139ae35797f6ae84da0c20
Lily Cov <lily@bettersites.com>
2021-08-06 11:40:27 -0700

    Finshed log search. Need to check it then go to next.

--- src/_05_Intro_To_Hash_Maps/_02_LogSearch.java
+++ src/_05_Intro_To_Hash_Maps/_02_LogSearch.java
@@ -3,9 +3,11 @@ package _05_Intro_To_Hash_Maps;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
+import java.util.Set;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class _02_LogSearch implements ActionListener {
@@ -43,6 +45,11 @@ public class _02_LogSearch implements ActionListener {
 
 	HashMap <Integer, String> IDlog = new HashMap <Integer, String>();
 	
+	JButton add;
+	JButton search;
+	JButton view;
+	JButton remove;
+	
 	public void createGUI() {
 		JFrame frame  = new JFrame();
 		JPanel panel = new JPanel();
@@ -50,10 +57,10 @@ public class _02_LogSearch implements ActionListener {
 		frame.setTitle("Log Search");
 		frame.add(panel);
 		
-		JButton add = new JButton();
-		JButton search = new JButton();
-		JButton view = new JButton();
-		JButton remove = new JButton();
+		add = new JButton();
+		search = new JButton();
+		view = new JButton();
+		remove = new JButton();
 		
 		add.setText("Add Entry");
 		search.setText("Search by ID");
@@ -81,6 +88,66 @@ public class _02_LogSearch implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
+		
+		
+		//ADD ENTRY---------------------------------------------------------
+		if(e.getSource().equals(add)) {
+			System.out.println("add is working");
+			String ID = JOptionPane.showInputDialog("Enter an ID number");
+			int numID = Integer.parseInt(ID);
+			String name  = JOptionPane.showInputDialog("Enter a name");
+			IDlog.put(numID, name);
+		}
+		
+		
+		//SEARCH BY ID------------------------------------------------------
+		else if(e.getSource().equals(search)) {
+			System.out.println("search is working");
+			String searchID = JOptionPane.showInputDialog("Enter an ID number");
+			int numSearchID = Integer.parseInt(searchID);
+			
+			if(IDlog.containsKey(numSearchID)) {
+				
+				JOptionPane.showMessageDialog(null, IDlog.keySet() + ",  " + IDlog.get(numSearchID), "ID search results", JOptionPane.PLAIN_MESSAGE);
+				//JOptionPane.showmess
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "The entry " + "'" + searchID + "'" + " does not exist", "ID search results", JOptionPane.ERROR_MESSAGE);
+			}
+			
+		}
+		
+		
+		//VIEW LIST--------------------------------------------------------
+		else if(e.getSource().equals(view)) {
+			System.out.println("view is working");
+			String holder = "";
+			
+			for(Integer i: IDlog.keySet()) {
+				holder += "ID: "+ i + " ,  Name: " + IDlog.get(i) + "\n";
+			
+			}
+			
+			JOptionPane.showMessageDialog(null, holder, "View List", JOptionPane.PLAIN_MESSAGE);
+			
+		}
+		
+		
+		//REMOVE ENTRY-------------------------------------------------------
+		else{
+			System.out.println("Remove is working");
+			
+			String removeID = JOptionPane.showInputDialog("Enter an ID number");
+			int numRemoveID = Integer.parseInt(removeID);
+			
+			if(IDlog.containsKey(numRemoveID)) {
+				IDlog.remove(numRemoveID);
+				JOptionPane.showMessageDialog(null, "ID removed sucessfully", "Remove Entry", JOptionPane.INFORMATION_MESSAGE);
+			}
+			else {
+				JOptionPane.showMessageDialog(null, "The entry " + "'" + removeID + "'" + " does not exist", "ID search results", JOptionPane.ERROR_MESSAGE);
+			}
+		}
 		
 	}
 	
