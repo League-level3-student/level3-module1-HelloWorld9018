1a3e4b5d99933d843049094463d492632a93ed61
Lily Cov <lily@bettersites.com>
2021-09-02 17:20:52 -0700

    Hi.

--- src/_07_California_Weather/CaliforniaWeather.java
+++ src/_07_California_Weather/CaliforniaWeather.java
@@ -1,6 +1,13 @@
 package _07_California_Weather;
 
+import java.awt.CardLayout;
 import java.util.HashMap;
+
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /*
  * OBJECTIVE:
@@ -41,9 +48,67 @@ public class CaliforniaWeather {
         } else {
             System.out.println(cityName + " is " + datum.weatherSummary + " with a temperature of " + datum.temperatureF + " F");
         }
+        
+        createGUI();
     }
     
+    JPanel panels;
+    
+    final static String TEXTPANEL = "Card with JTextField";
+    final static String BUTTONPANEL = "Card with JButtons";
+    final static String SEARCHPANEL = "Card with search button";
+    
+    
     void createGUI() {
+    	
+    	//initailizing Jcomponents
+    	JFrame frame  = new JFrame("California Weather");
+    	frame.setVisible(true);
+    	
+    	panels = new JPanel();
+    	JLabel label = new JLabel("Filter results by...");
+    	
+    	JPanel panelText = new JPanel();
+    	JPanel panelButtons = new JPanel();
+    	JPanel panelSearch = new JPanel();
+    	
+    	JButton button1 = new JButton("City");
+    	JButton button2 = new JButton("Weather Condition");
+    	JButton button3 = new JButton("Temperature");
+    	JButton button4 = new JButton("SEARCH");
+    	
+    	//titling Jcompononets
+    	/*frame.setTitle("California Weather");
+    	
+    	label.setText("Filter results by...");
+    	
+    	button1.setText("City");
+    	button2.setText("Weather Condition");
+    	button3.setText("Temperature");
+    	
+    	button4.setText("SEARCH");*/
+    	
+    	//Adding JComponents
+  
+    	frame.add(panels);
+    	//panels.setLayout(new BoxLayout(frame, BoxLayout.Y_AXIS));
+    	panels.add(label);
+    	panels.add(button1);
+    	panels.add(button2);
+    	panels.add(button3);
+    	panels.add(button4);
+    	
+   /* 	panels.add(panelText, TEXTPANEL);
+    	panels.add(panelButtons, BUTTONPANEL);
+    	panels.add(panelSearch, SEARCHPANEL);*/
+    	
+    	frame.pack();
+    	//panels.setLayout(new CardLayout());
+
+    	
+    	
+    	//frame.pack();
+    	
     	//option dialog
     	//or button/gui
     }
